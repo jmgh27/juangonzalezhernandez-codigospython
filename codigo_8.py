7e1d0ce15f7f86b9f4c9bd131ceb172b724dc5eb
jmgh27 <85376330+jmgh27@users.noreply.github.com>
2021-09-13 15:13:11 -0500

    Update codigo_8.py

--- codigo_8.py
+++ codigo_8.py
@@ -1,4 +1,6 @@
-for i in range(5):
-    print("* "*(0+i))
-for i in range(5):
-    print("* "*(5-i))
+pir = 5
+for i in range(pir):
+    print("* "*(0+i)) 
+    if i == 4 :
+        for i in range(5):
+            print("* "*(5-i))
