14abb01f57d351b7bf1d3bd87c95768b1e023e40
jmgh27 <85376330+jmgh27@users.noreply.github.com>
2021-09-13 14:00:25 -0500

    Create codigo_6.py

---
+++ codigo_6.py
@@ -0,0 +1,8 @@
+num = input("Numero a comparar: ")
+num = int(num)
+if   num > 0:
+    print("Su numero es positivo")
+elif num < 0:
+    print("Su numero es negativo")
+elif dat == 0:
+    print("Su numero es cero")
